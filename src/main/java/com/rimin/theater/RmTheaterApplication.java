dc70120801c790b72bc1ae5070dded8bfe231e42
Home <chris0540@naver.com>
2024-06-18 15:44:27 +0900

    - 권민석 rm_theater 만들기 초기 설정

---
+++ src/main/java/com/rimin/theater/RmTheaterApplication.java
@@ -0,0 +1,15 @@
+package com.rimin.theater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
+public class RmTheaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmTheaterApplication.class, args);
+	}
+
+}
